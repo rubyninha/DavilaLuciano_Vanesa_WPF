b1cddf0b9ba0a62696e8e2b8c6f8aeac7e2e58af
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-04-09 12:22:51 -0400

    Initial setup of Values and Variables exercises how to make different types of variables with variable declarations

---
+++ Exercises/String_Boolean Variables/js/script.js
@@ -0,0 +1,9 @@
+// Variables of Different Types, Numbers, Booleans and Strings
+
+var age; //Number variable
+
+var isSudent = fale; //Boolean Variable
+
+var firstName - "Vanesa";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
