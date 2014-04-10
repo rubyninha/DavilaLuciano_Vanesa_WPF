2713660c213849a17382e1f327f9f04ec13acc80
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-04-10 00:10:20 -0400

    Initial commit of GoTo Week 2 Training

---
+++ Week 2 Training/js/script.js
@@ -0,0 +1,46 @@
+// JavaScript File
+
+//Figure Out How Old We Currently Are
+// & How Old We Will Be In A Given Amount Of Years
+
+//Prompt user for name
+
+var name = prompt("Please type in your name.");
+
+console.log(name);
+
+//Create an alert that the user will see
+
+alert("Welcome " +name+"! We have one more question for you.");
+
+//Get the users birth year
+var yearBorn = prompt("What year were you born?");
+
+console.log(yearBorn);
+
+//Variable for current Year
+var currentYear = 2014;
+
+//Figure out how old the user is
+//Create a variable to hold this answer
+
+var age = currentYear - yearBorn;
+
+console.log(age);
+
+//Alert the user on how old they are
+alert(name+" you are "+age+" years old.");
+
+
+//Lets make it more complex
+
+//Ask the user for a future time.
+var yearsMore = prompt("How many years in the future do you want to check?");
+
+console.log(yearsMore);
+
+//Calculate our future age
+
+var futureAge = age + yearsMore;
+
+console.log(futureAge);
