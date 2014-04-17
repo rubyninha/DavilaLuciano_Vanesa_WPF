cf98f3135c6066e2968b5e780f36bc7ae1a3f506
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-04-17 17:50:09 -0400

    Added an "else" statement

--- Exercises/Conditional Logic - with an Expression/js/script.js
+++ Exercises/Conditional Logic - with an Expression/js/script.js
@@ -1,12 +1,15 @@
-// Conditional Logic - with an Expressions
+// Conditional Logic - with an Expression
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
+}
+else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
 }
