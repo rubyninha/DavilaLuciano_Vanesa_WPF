01f0a6511e5b3a2bdd6393d7580224927c594fb8
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-17 19:48:44 -0400

    Screencast: Arithmetic Operators

---
+++ Activities/Week 1/Arithmetic Operators/js/script.js
@@ -0,0 +1,14 @@
+/* 
+Vanesa Dávila-Luciano
+WPF 1407 Section 02
+GoTo Training Example Week #2
+*/
+
+
+// Expressions - Arithmetic Operators
+
+//area of the triangle is half of the width times the height
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area);
