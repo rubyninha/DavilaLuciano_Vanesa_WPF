0bbc66e1ff99d03d82edcfc4c792a873a86de8cb
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-17 19:31:44 -0400

    Screencast: String and Boolean Variables When working with text or "true" or "false" storage, Strings and Booleans are essential.

---
+++ Activities/Defining Variables/js/script.js
@@ -0,0 +1,16 @@
+/* 
+Vanesa Dávila-Luciano
+WPF 1407 Section 02
+Screencast: String and Boolean Variables
+*/
+
+
+// Defining Variables
+
+var age; //declaration
+age = 38; //definition
+
+//storage container - value
+
+var yearOfBirth = 1970;
+ //declaring and defining on the same line
