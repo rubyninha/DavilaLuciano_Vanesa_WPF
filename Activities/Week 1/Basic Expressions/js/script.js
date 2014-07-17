d2cf2fa690c3795940a71e1bec2eabfc6351b646
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-17 19:44:45 -0400

    Screencast: Basic Expressions

---
+++ Activities/Week 1/Basic Expressions/js/script.js
@@ -0,0 +1,12 @@
+/* 
+Vanesa Dávila-Luciano
+WPF 1407 Section 02
+Screncast: Basic Expressions 
+*/
+
+// Expressions
+
+var a = 2; //sets up our variable a and defines it with 2
+var b;
+b = a + 3; //add 3 to a
+console.log(a);
