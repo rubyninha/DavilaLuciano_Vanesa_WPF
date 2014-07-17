a05701f528ca697ca0ea929cfd9afb2c27c78b09
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-17 19:44:00 -0400

    Created a new directory for Activities of Week 1

---
+++ Activities/Week 1/Arrays/js/script.js
@@ -0,0 +1,16 @@
+/* 
+Vanesa Dávila-Luciano
+WPF 1407 Section 02
+Screencast: Arrays
+*/
+
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
+
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+var num = 1;
+
+console.log(muppetNames[num]); //lets me see what's in the indez #1
