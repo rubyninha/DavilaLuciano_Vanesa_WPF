307ac521a2ab62bdcb183b42131003e5a1fcea2d
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-17 19:59:33 -0400

    Screencast: Assignment Operators

---
+++ Activities/Week 2/Order of Operations/js/script.js
@@ -0,0 +1,22 @@
+/* 
+Vanesa Dávila-Luciano
+WPF 1407 Section 02
+Screencast: Order of Operations 
+*/
+
+
+// Expressions - PEMDAS - The Order of Operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
+
+var length = 7;
+var width = 6;
+var permimeter = length * 2 + width * 2;
+console.log(permimeter);
