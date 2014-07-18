f4b09288c33bd50dd34b8176aad380532d41a71e
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-17 23:46:46 -0400

    Created a prompt for user to provide ounces of milk toddler drinks in a nap. Printed with console.log

--- Expressions/Expressions_Wacky/js/script.js
+++ Expressions/Expressions_Wacky/js/script.js
@@ -5,5 +5,22 @@ Assignment: Expressions_Wacky
 */
 
 
-//Find out how bottles of milk will take a toddler to fall asleep
+//Find out how many ounces of milk will take a toddler to fall asleep for a full night (8 hours on average).
+
+
+//Prompt user for name
+
+var name = prompt("Please, type in your name.");
+
+console.log(name);
+
+//Create an alert that the user will see
+alert("Welcome, " +name+"! Let's figure out how many ounces of milk will take for your toddler to fall asleep for the night...");
+
+
+//Prompt user for length in pixels of the images
+
+var nap = prompt("How many ounces does your toddler drink before a 2 hour nap?");
+console.log(nap);
+
 
