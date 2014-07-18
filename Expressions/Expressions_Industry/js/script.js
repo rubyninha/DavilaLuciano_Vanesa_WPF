4bf23da3a94510ade5e61387b866f502a27bcc3a
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-17 21:40:54 -0400

    Initial setup of Expressions Assignment, Industry for index.html and script.js

---
+++ Expressions/Expressions_Industry/js/script.js
@@ -0,0 +1,9 @@
+/* 
+Vanesa Dávila-Luciano
+WPF 1407 Section 02
+Assignment: Expressions_Idustry
+*/
+
+
+//Figure the size of a website banner containing three images, based on the sum of the pixels of each image horizontally or vertically
+
