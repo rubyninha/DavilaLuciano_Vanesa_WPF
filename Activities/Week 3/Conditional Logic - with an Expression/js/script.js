e804ef76e5a781febcf1e9a90856a871ddad8e57
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-24 17:01:38 -0400

    Week 3 Activity - Screencast: Conditional Logic - with an Expression

---
+++ Activities/Week 3/Conditional Logic - with an Expression/js/script.js
@@ -0,0 +1,12 @@
+// Conditional Logic - with an Expression
+
+var kidHeight = 52;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
