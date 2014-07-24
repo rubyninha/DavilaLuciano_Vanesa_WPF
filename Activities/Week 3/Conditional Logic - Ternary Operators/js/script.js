3df66be171709126115db89d96ddb604a94e16fb
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-24 18:45:29 -0400

    Week 3 Activity - Screencast: Conditional Logic - Ternary Operators

---
+++ Activities/Week 3/Conditional Logic - Ternary Operators/js/script.js
@@ -0,0 +1,21 @@
+// Conditional Logic - Ternary Operators
+
+/*var gpa = 48;
+
+//if the gpa is over the min 2.0 score, the student can graduate
+if(gpa > 2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA is too low!");
+
+}
+
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");*/
+
+
+var age = 11;
+var book;
+
+//If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
