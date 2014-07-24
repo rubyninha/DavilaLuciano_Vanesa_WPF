e7c7aba9517765cb4e659ca562165aa1efd888c0
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-24 13:23:09 -0400

    Create an array of fruit

--- GoToTraining Week3/js/gotoweek3.js
+++ GoToTraining Week3/js/gotoweek3.js
@@ -5,3 +5,19 @@ GoTo Training Example Week #3
 */
 
 //alert("Testing 1,2,3");
+
+//Code that is going to check how many pears are in a fruit bowl
+
+//Create our "fruit bowl""
+//Array - hold multiple variables
+
+var bowlOfFruit = ["apple", "banana", "pear", "peach", "pear"];
+console.log(bowlOfFruit);
+
+//How can I get one fruit in the bowl?
+console.log(bowlOfFruit[1]);
+
+//Array length
+//Dot Syntax - "use a period""
+//Tell us how many items are in the array
+console.log(bowlOfFruit.length);
