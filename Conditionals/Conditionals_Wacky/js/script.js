240f4b8a5736e42e3d8fcacab7d396c69441ab1a
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-26 15:45:23 -0400

    Create user prompt for name

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -5,3 +5,8 @@ Assignment: Conditionals_Wacky
 */
 
 // Conditionals - Wacky Assignment
+
+//Determine which nail polish color to wear according to destination
+
+//Prompt user for name
+var name = prompt("Please, type in your name:");
