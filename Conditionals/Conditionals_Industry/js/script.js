5bd32ce424966a1fc5691101716b10414577db11
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-26 20:40:44 -0400

    Test if variable is blank. Created reprompt for user to input name. Printed in console.log

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -10,4 +10,12 @@ Assignment: Conditionals_Industry
 //Prompt user for name
 var name = prompt("Please, type in your name:");
 
+//Test if the variable is blank
+
+if(name===""){
+    //The user left it blank
+
+    //Reprompt the user for name
+    name = prompt("Please, type in your name. Do not leave blank.");
+}
 console.log(name);
