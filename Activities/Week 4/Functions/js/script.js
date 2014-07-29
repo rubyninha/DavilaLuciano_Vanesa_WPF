8a86e2678c83489accd4fbcff11f1500bd1d38ca
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-29 16:46:37 -0400

    Week 4 Activity - Screencast: Basic Function Structure

---
+++ Activities/Week 4/Functions/js/script.js
@@ -0,0 +1,19 @@
+/*
+ Vanesa Dávila-Luciano
+ WPF 1407 Section 02
+ Screencast: Basic Function Structure
+ */
+
+// Functions: Basic Structure
+
+function outptMsg() {
+    console.log("Hello, World");
+
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area =  width * height;
+    console.log(area);
+}
