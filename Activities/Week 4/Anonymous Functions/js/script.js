c984dda352108c861e9e2946fe60e8cfe5805cd0
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-29 18:01:17 -0400

    Week 4 Activity - Screencast: Anonymous Functions

---
+++ Activities/Week 4/Anonymous Functions/js/script.js
@@ -0,0 +1,22 @@
+/*
+ Vanesa Dávila-Luciano
+ WPF 1407 Section 02
+ Screencast: Anonymous Functions
+ */
+
+// Functions - Anonymous Functions
+
+
+var calcArea = function(width, height){//defining
+    //code the function runs
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 30);//invoking
+
+console.log(a);
+
+
+
+
