e9cb467ff355371108f777f807faf3f227f6f325
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-29 17:36:02 -0400

    Week 4 Activity - Screencast: Parameters and Arguments

---
+++ Activities/Week 4/Parameters and Arguments/js/script.js
@@ -0,0 +1,33 @@
+/*
+ Vanesa Dávila-Luciano
+ WPF 1407 Section 02
+ Screencast: Parameters and Arguments
+ */
+
+// Functions - Parameters and Arguments
+
+
+/*calcArea(30, 20);
+
+function calcArea(w, h) {//w=30, h=20
+    var area = w * h;
+    console.log(area);
+}*/
+
+function dogYears(age){//parameters
+    var dogYears = age * 7;
+    console.log("Sparky is "+dogYears+" years old.")
+}
+var age1 = 4;
+dogYears(age1);//arguments
+dogYears(5);
+
+
+
+
+
+
+
+
+
+
