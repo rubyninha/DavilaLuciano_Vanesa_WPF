f669e16dfaf7dd9cbe0a2d9f11df6a1609acbdc3
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-29 18:11:13 -0400

    Week 4 Activity - Screencast: While Loop

---
+++ Activities/Week 4/Loops/js/script.js
@@ -0,0 +1,16 @@
+/*
+ Vanesa Dávila-Luciano
+ WPF 1407 Section 02
+ Screencast: While Loop
+ */
+
+// Loops - While Loop
+
+console.log('------ Loops ------');
+
+var b = 50; //setups the index
+
+while (b > 0){ //checks the condition
+    console.log(b + "kegs on the wall");
+    b--; //increments or decrements
+}
