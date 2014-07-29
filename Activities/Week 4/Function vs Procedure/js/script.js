796b0b04add37fa6e67b9fcad283b84d0e71ac7b
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-29 17:50:48 -0400

    Week 4 Activity - Screencast: Function vs Procedure

---
+++ Activities/Week 4/Function vs Procedure/js/script.js
@@ -0,0 +1,26 @@
+/*
+ Vanesa Dávila-Luciano
+ WPF 1407 Section 02
+ Screencast: Function vs Procedure
+ */
+
+// Functions - Function vs Procedure
+
+
+//This is a function
+function calcArea(width, height){
+    var area = width * height;
+    return area;
+}
+
+//This is a procedure
+function calcArea(width, height){
+    var area = width * height;
+    console.log(area);//no return
+}
+
+
+
+
+
+
