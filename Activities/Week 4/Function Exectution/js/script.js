456518b6dbd8d16790ff546eebdae4777c498ca7
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-29 16:54:36 -0400

    Week 4 Activity - Screencast: Function Invocation

---
+++ Activities/Week 4/Function Exectution/js/script.js
@@ -0,0 +1,23 @@
+/*
+ Vanesa Dávila-Luciano
+ WPF 1407 Section 02
+ Screencast: Function Invocation
+ */
+
+// Function Invocation
+
+function outptMsg() {
+    console.log("Hello, World");
+
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area =  width * height;
+    console.log(area);
+}
+
+calcArea();
+calcArea();
+calcArea();
