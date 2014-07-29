11e7c9c9b45b25d27b88a3708a4238379dd315b4
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-29 17:43:43 -0400

    Week 4 Activity - Screencast: Returning Values

---
+++ Activities/Week 4/Returning Values/js/script.js
@@ -0,0 +1,21 @@
+/*
+ Vanesa Dávila-Luciano
+ WPF 1407 Section 02
+ Screencast: Returning Values
+ */
+
+// Functions - Returning Values
+
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h) {
+    var area = w * h;
+    return area;//function spitting the info out
+}
+console.log(total);
+
+
+
+
+
