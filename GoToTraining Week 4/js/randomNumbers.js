ab6f798f4c520d7c9d4cb6b1127128bb039af838
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-07-30 15:25:23 -0400

    Setup While Loop Validation

--- GoToTraining Week 4/js/randomNumbers.js
+++ GoToTraining Week 4/js/randomNumbers.js
@@ -6,3 +6,43 @@
 
 //alert("Testing 1,2,3");
 
+//Create a file that will give us a random number
+
+//Ask the user for a min number
+
+var min = prompt("Please type in a minimum number:");
+
+//Validate that prompt
+
+//before use a conditional - OLD WAY
+
+/*if(min ===""){
+    //Re-prompt the user for a number
+    min = prompt("Please type in a minimum number. Please don't leave blank:");
+}
+
+
+// Validate using a while loop
+// Test for an empty string, a.k.a. user didn't type anything in
+while(min===""){
+    //Re-prompt the user
+    min = prompt("Please type in a minimum number:");
+
+}
+
+// Validate that it is a number
+while(isNaN(min)){
+    //Re-prompt the user
+    min = prompt("Please type in a minimum number:");
+
+}
+ */
+
+// Combine validation into one while loop
+while(isNaN(min) || min===""){
+    //Re-prompt the user
+    min = prompt("Please type in a minimum number. Inside of while");
+
+}
+
+
