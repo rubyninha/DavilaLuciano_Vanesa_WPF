e58c097f9d706dc21efe2b80680b8092830139ec
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-08-04 14:27:38 -0400

    Prompted user for an hourly rate. Printed with console.log

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -5,3 +5,10 @@ Assignment: Functions_Industry
 */
 
 // Functions - Industry Assignment
+
+//Create a function that will calculate the amount a web designer will charge a client by the hours they have worked
+
+//Ask the user for their hourly pay rate
+
+var hourRate = prompt("Please, enter your hourly rate:");
+console.log(hourRate);
