3b1a75071d8804ebf0bfbf16580beebe4a2144b1
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-08-04 16:07:49 -0400

    Create prompt for user input

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -7,9 +7,10 @@ Assignment: Functions_Wacky
 // Functions - Wacky Assignment
 
 
+// Create a function that will calculate how many bunnies a female rabbit could have in a lifetime
 
 
+//Prompt user to give age of their female rabbit
 
-
-
+var yourBunny = prompt("Let's calculate how many bunnies your rabbit could have. Enter age of the female rabbit:");
 
