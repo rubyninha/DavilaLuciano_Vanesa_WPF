a7fe4fdd49df5b2756b49c8081bec98d7ad31ae6
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-08-04 16:11:04 -0400

    Validate prompt with a while loop

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -14,3 +14,9 @@ Assignment: Functions_Wacky
 
 var yourBunny = prompt("Let's calculate how many bunnies your rabbit could have. Enter age of the female rabbit:");
 
+// Validate using while loop
+while(isNaN(yourBunny) || yourBunny==="") {
+    //Re-prompt the user
+    yourBunny = prompt("Please, enter your bunny's age in form of a number.");
+}
+
